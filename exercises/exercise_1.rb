d032ba322b7693e3f2f936cac1ff2537b18b2f9c
Adekunle Oshodi <kunleoshodi856@gmail.com>
2023-10-03 18:57:10 -0500

    delete third store

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -9,3 +9,5 @@ end
 Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
 Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true)
 Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+
+puts "No. of stores: #{Store.count}"
--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -5,4 +5,11 @@ require_relative './exercise_2'
 puts "Exercise 3"
 puts "----------"
 
-# Your code goes here ...
+class Store < ActiveRecord::Base
+end
+
+@store3 = Store.find(3)
+
+@store3.destroy
+
+puts "No. of stores: #{Store.count}"
