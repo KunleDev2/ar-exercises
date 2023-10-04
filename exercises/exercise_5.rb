1c98281d4e6dc3b65895b6078db4d31f0f64daf5
Adekunle Oshodi <kunleoshodi856@gmail.com>
2023-10-03 19:58:36 -0500

    Calculate revenues

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,14 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+class Store < ActiveRecord::Base
+end
+
+total_revenue = Store.sum(:annual_revenue)
+puts "Total Revenue: $#{total_revenue}"
+
+average_annual_revenue = Store.average(:annual_revenue)
+puts "Average Annual Revenue: $#{average_annual_revenue}"
+
+high_revenue_stores = Store.where('annual_revenue >= ?', 1000000).count
+puts "No. of Stores Generating Greater than or equal to $1M: #{high_revenue_stores}"
