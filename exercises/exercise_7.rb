225d1f4bc71a761b02bd4876867c14d51bd0c0c3
Adekunle Oshodi <kunleoshodi856@gmail.com>
2023-10-04 13:02:07 -0500

    Implement One-to-many association

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -5,8 +5,20 @@ require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
+require_relative '../lib/store'
+require_relative '../lib/employee'
 
 puts "Exercise 7"
 puts "----------"
 
-# Your code goes here ...
+puts "Enter a store name:"
+store_name = gets.chomp
+
+new_store = Store.new(name: store_name)
+
+if new_store.save
+  puts "Saved successfully!"
+else
+  puts "Errors:"
+  new_store.errors.full_messages.each { |msg| puts msg }
+end
